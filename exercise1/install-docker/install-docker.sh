06183dd8335290e6225c9a552c65f0f94403bf6e
Ubuntu <ubuntu@ubuntu1.merel107.local>
2021-08-16 09:52:10 +0000

    Renewed jenkins-course

---
+++ exercise1/install-docker/install-docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update sources and apps
+apt update -y && apt upgrade -y
+
+# Remove docker if it is already installed
+apt remove -y docker docker-engine docker.io containerd runc
+
+# Install docker pre-requisites
+apt install -y curl apt-transport-https ca-certificates curl gnupg lsb-release
+
+# Install docker engine, using the install script
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+# add user to docker group
+if [ "$(lsb_release -is)" == "Ubuntu" ]; then
+  usermod -aG docker ubuntu
+else
+  echo "Add user to \"docker\" group."
+fi
+
+# Install docker-compose
+apt install -y docker-compose
+
+# create /var/jenkins_home
+mkdir -p /var/jenkins_home
+
+# Set permissions
+chown -R 1000:1000 /var/jenkins_home
